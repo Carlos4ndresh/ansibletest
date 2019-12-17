f0e5a25bbf89c177f065d585e6e37c567890e92c
Carlos Andrés Herrera <carlos.herrera@outlook.com>
2019-12-16 22:00:17 -0500

    test of install requirements

--- app/routes.py
+++ app/routes.py
@@ -9,8 +9,13 @@ def index():
         name = request.form.get('name')
         favorite_color = request.form.get('color')
         cat_or_dog = request.form.get('animal')
-        entry = models.Preferences(name=name, favorite_color=favorite_color, cat_or_dog=cat_or_dog)
+        entry = models.Preferences(
+            name=name, favorite_color=favorite_color, cat_or_dog=cat_or_dog)
         db.session.add(entry)
         db.session.commit()
     prefs = models.Preferences.query.all()
     return render_template('index.html', preferences=prefs)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
